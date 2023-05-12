42ae22539513abd4ac113cdb36ca437e139aa7b8
DataEngel <miguel.program.73@gmail.com>
2023-05-11 22:53:35 -0600

    new test1

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,11 @@
-from hello import add
+from hello import toyou, add, subtract
 
-def test_add():
-    assert add(1,2) == 3
-    
+
+def setup_function(function):
+    print(" Running Setup: %s " % function.__name__)
+    function.x = 10
+
+
+def teardown_function(function):
+    print(" Running Teardown:%s" % function.__name__)
+    del function.x
